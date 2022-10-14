24b1d00255bfad51025f2d70e873749ca65d4e83
AdnanHaider72 <108249226+AdnanHaider72@users.noreply.github.com>
2022-10-14 12:43:03 +0900

    Add files via upload

---
+++ FishSeg/PFFSnet_DeepFish/PFFSnet_DeepFish_Train.m
@@ -0,0 +1,97 @@
+
+close all
+clear all
+
+load 'PFFSnet_DeepFish_lgraph'
+lgraph=lgraph_2;
+
+Folder ='';
+train_img_dir = fullfile(Folder,'Train');
+imds = imageDatastore(train_img_dir); 
+% %  
+Val_img_dir = fullfile(Folder,'Val');
+imdsVal = imageDatastore(Val_img_dir); 
+
+classes = ["FISH","BG"]; 
+labelIDs   = [255,0]; 
+
+train_label_dir = fullfile(Folder,'Train_GT');  
+pxds = pixelLabelDatastore(train_label_dir,classes,labelIDs);
+
+Val_label_dir = fullfile(Folder,'Val_GT');  
+pxdsVal = pixelLabelDatastore(Val_label_dir,classes,labelIDs);
+
+tbl = countEachLabel(pxds); 
+
+
+frequency = tbl.PixelCount/sum(tbl.PixelCount); 
+
+imageFreq = tbl.PixelCount ./ tbl.ImagePixelCount;
+classWeights = median(imageFreq) ./ imageFreq;    
+
+%%% Training options %%%%%
+
+dsVal = combine(imdsVal,pxdsVal);
+
+options = trainingOptions('adam', ...
+    'SquaredGradientDecayFactor',0.95, ...
+    'GradientThreshold',5, ...
+    'GradientThresholdMethod','global-l2norm', ...
+    'Epsilon',1e-5, ...
+    'InitialLearnRate',1e-3, ...
+    'L2Regularization',0.0005, ...
+    'ValidationData',dsVal,...
+    'MaxEpochs',130, ...  
+    'MiniBatchSize',6, ...
+    'CheckpointPath',tempdir, ...
+    'Shuffle','every-epoch', ...
+    'VerboseFrequency',2, ...
+    'OutputFcn',@(info)stopIfAccuracyNotImproving(info,25));
+    
+
+augment_data = imageDataAugmenter('RandXReflection',true,...
+    'RandXTranslation',[-2 2],'RandYTranslation',[-2 2]); 
+
+
+training_data = pixelLabelImageDatastore(imds,pxds,...
+    'DataAugmentation',augment_data); 
+
+
+[net, info] = trainNetwork(training_data,lgraph,options);% 
+
+function stop = stopIfAccuracyNotImproving(info,N)
+
+stop = false;
+
+persistent bestValAccuracy
+persistent valLag
+
+if info.State == "start"
+    bestValAccuracy = 0;
+    valLag = 0;
+    
+elseif ~isempty(info.ValidationLoss)
+    
+    % Compare the current validation accuracy to the best accuracy so far,
+    % and either set the best accuracy to the current accuracy, or increase
+    % the number of validations for which there has not been an improvement.
+    if info.ValidationAccuracy > bestValAccuracy
+        valLag = 0;
+        bestValAccuracy = info.ValidationAccuracy;
+    else
+        valLag = valLag + 1;
+    end
+    
+    % If the validation lag is at least N, that is, the validation accuracy
+    % has not improved for at least N validations, then return true and
+    % stop training.
+    if valLag >= N
+        stop = true;
+    end
+    
+end
+
+end
+
+  
+
